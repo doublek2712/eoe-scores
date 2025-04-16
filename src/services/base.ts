97ceeff124e5861171f426c7603ccfd31b88ee53
DoubleK <qk.khai2712@gmail.com>
2025-04-16 21:27:47 +0700

    adjust api url

--- src/services/base.ts
+++ src/services/base.ts
@@ -1,3 +1,3 @@
 const location = window.location;
 const domain = `${location.protocol}//${location.hostname}`;
-export const BASE_URL = `${domain}:3000`;
+export const BASE_URL = `${domain}:3000/api/json`;
